ba19928422db0f10d6337fc1a769c0585b6f1330
DjDCH <coding@djdch.com>
2017-04-02 14:59:13 -0400

    Fixes noninteractive enforcement

--- updatewrapper/flavor/debian.py
+++ updatewrapper/flavor/debian.py
@@ -3,6 +3,9 @@ from ansible.module_utils._text import to_bytes
 from updatewrapper.flavor import FlavorBase
 
 
+ENV_VARS = 'DEBIAN_FRONTEND=noninteractive APT_LISTCHANGES_FRONTEND=text '
+
+
 class Debian(FlavorBase):
     def __init__(self, host, dist_upgrade=False, **kwargs):
         self.command = 'dist-upgrade' if dist_upgrade else 'upgrade'
@@ -10,13 +13,13 @@ class Debian(FlavorBase):
         super().__init__(host)
 
     def update_cache(self):
-        return self.host.run('DEBIAN_FRONTEND=noninteractive apt-get update')
+        return self.host.run(ENV_VARS + 'apt-get update')
 
     def check_update(self):
-        return self.host.run('DEBIAN_FRONTEND=noninteractive apt-get --show-upgraded --assume-no ' + self.command)
+        return self.host.run(ENV_VARS + 'apt-get --show-upgraded --assume-no ' + self.command)
 
     def has_update(self, returncode, stdout, stderr):
         return to_bytes('The following packages will be upgraded') in stdout
 
     def perform_update(self):
-        return self.host.run('DEBIAN_FRONTEND=noninteractive apt-get --show-upgraded --assume-yes ' + self.command)
+        return self.host.run(ENV_VARS + 'apt-get --show-upgraded --assume-yes ' + self.command)
